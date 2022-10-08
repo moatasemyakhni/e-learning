57320bd736bd0062f9ee5aa65f70ca32b0c54c65
= <motasem.yakhni@gmail.com>
2022-10-08 13:00:35 +0300

    Frontend => create login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -23,6 +23,28 @@ const Login = () => {
   return (
     <div>
         <p ref={errorRef} className={errorMessage ? "error-msg": "view-none"} aria-live="assertive">{errorMessage}</p>
+        <form>
+            <label htmlFor="email">Email:</label>
+            <input
+                type="text"
+                id="email"
+                ref={emailRef}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+             />
+
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+             />
+
+             <button>Sign In</button>
+        </form>
     </div>
   )
 }
