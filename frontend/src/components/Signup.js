f8c9056ebcf5ab6730ac34a0fa5161ea3e353351
= <motasem.yakhni@gmail.com>
2022-10-08 16:06:14 +0300

    Frontend => prepare signuo input states

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,6 +2,23 @@ import {useRef, useState, useEffect} from 'react';
 
 
 function Signup() {
+    // set focus on load
+    const fullNameRef = useRef();
+    const errorRef = useRef();
+    
+    const [fullName, setFullName] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [fullNameFocus, setFullNameFocus] = useState(false);
+
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [validPassword, setValidPassword] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
+
+    
   return (
     <>
         <div>Register</div>
