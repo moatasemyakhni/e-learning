e1a84917848465a462c666db4767f2f107247a4e
= <motasem.yakhni@gmail.com>
2022-10-08 13:11:34 +0300

    Frontend => create login submit logic

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -20,34 +20,58 @@ const Login = () => {
         setErrorMessage('');
     }, [email, password]);
 
+    const loginSubmit = async (e) => {
+        e.preventDefault();
+        // axios here
+        console.log(email, password);
+
+        // clear form
+        setEmail('');
+        setPassword('');
+        setSuccess(true);
+    }
+
   return (
-    <div>
-        <p ref={errorRef} className={errorMessage ? "error-msg": "view-none"} aria-live="assertive">{errorMessage}</p>
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input
-                type="text"
-                id="email"
-                ref={emailRef}
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                required
-             />
+    <>
+    {/* if we logged in successfully */}
+    {sucess ? (
+        <section>
+            <h1>You are being redirected</h1>
+            <p>
+                Waiting...
+            </p>
+        </section>
+    ) : ( 
+        <div>
+            <p ref={errorRef} className={errorMessage ? "error-msg": "view-none"} aria-live="assertive">{errorMessage}</p>
+            <form onSubmit={loginSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    ref={emailRef}
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                />
 
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                required
-             />
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
 
-             <button>Sign In</button>
-        </form>
-        <p>Need an Account?</p>
-        <a href="#">Sign Up</a>
-    </div>
+                <button>Login</button>
+            </form>
+            <p>Need an Account?</p>
+            {/* need a route */}
+            <a href="#">Sign Up</a>
+        </div>
+        )}
+    </>
   )
 }
 
