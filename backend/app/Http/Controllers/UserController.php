1e9a2c62cf9a73908fe8c0aa0157205014ce5125
= <motasem.yakhni@gmail.com>
2022-10-08 19:29:36 +0300

    Backend => add email checker method

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+    function checkEmail(Request $req) {
+        $userEmail = User::where('email', $req->email)->count();
+        if($userEmail === 0) {
+            return json_encode(['available' => true]);
+        }
+        return json_encode(['available' => false]);
+
+    }
 }
