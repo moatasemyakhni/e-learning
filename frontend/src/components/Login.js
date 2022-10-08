d75351b403512374c88acd57939225df49ef1491
= <motasem.yakhni@gmail.com>
2022-10-08 12:55:43 +0300

    Frontend => create error message

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from "react";
 
 const Login = () => {
     // set focus when error occur
-    const userRef = useRef();
+    const emailRef = useRef();
     const errorRef = useRef();
 
     const [email, setEmail] = useState('');
@@ -10,8 +10,20 @@ const Login = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const [sucess, setSuccess] = useState(false);
 
+    // set focus on email when loading
+    useEffect(() => {
+        emailRef.current.focus();
+    }, []);
+
+    // error will be cleared when change email or pwd
+    useEffect(() => {
+        setErrorMessage('');
+    }, [email, password]);
+
   return (
-    <div>Login</div>
+    <div>
+        <p ref={errorRef} className={errorMessage ? "error-msg": "view-none"} aria-live="assertive">{errorMessage}</p>
+    </div>
   )
 }
 
