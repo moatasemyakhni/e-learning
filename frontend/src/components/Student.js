d673263726e63416cd9b46d2674d7d570d8731c8
= <motasem.yakhni@gmail.com>
2022-10-11 21:56:11 +0300

    Frontend => clean Student component

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -1,56 +1,55 @@
 import Navbar from "./Navbar"
 import { baseUrl, userInfo, postApi, getApi} from "../scripts/utilities"
 import { useEffect, useState } from "react";
+
+
 const Student = () => {
+  const [name, setName] = useState('');
+  const [empty, setEmpty] = useState(true);
+  const [courses, setCourses] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [enrollCourse, setEnrollCourse] = useState(courses[0]);
 
-    const [name, setName] = useState('');
-    const [id, set_id] = useState(null);
-    const [empty, setEmpty] = useState(true);
-    const [courses, setCourses] = useState([]);
-    const [errorMessage, setErrorMessage] = useState(false);
-    const [enrollCourse, setEnrollCourse] = useState(courses[0]);
+  // get user info from token
+  const info = async () => {
+      const data = await userInfo(baseUrl, localStorage.getItem('user_token'))
+      setName(data.name);
+  }
 
-    const info = async () => {
-        const data = await userInfo(baseUrl, localStorage.getItem('user_token'))
-        setName(data.name);
-        set_id(data._id);
+  const getUnregisteredCourses = async () => {
+    const url = baseUrl + "/auth/non_registered_courses";
+    try {
+      const getCourses = await getApi(url, localStorage.getItem('user_token'));
+      setCourses(getCourses);
+    }catch(err) {
+      setEmpty(true);
+      setErrorMessage('Server is not responding'); 
     }
-    const getUnregisteredCourses = async () => {
-      const url = baseUrl + "/auth/non_registered_courses";
-      try {
+  }
 
-        const getCourses = await getApi(url, localStorage.getItem('user_token'));
-        setCourses(getCourses);
-      }catch(err) {
-        setEmpty(true);
-        setErrorMessage('Server is not responding'); 
-
-      }
-
-
+  const enroll = async (e) => {
+    let val;
+    if(!e || e === undefined) {
+      val = courses[0].code;
+    }else {
+      val = e.target.value;
     }
-    useEffect(() => {
-        info();
-        getUnregisteredCourses();
-    }, []);
+    const url = baseUrl + "/auth/student_register_course";
+    try {
+      const dataForm = new FormData();
+      dataForm.append('courses', val)
+      const post = await postApi(url, dataForm, localStorage.getItem('user_token'));
+    } catch(err) {
+      setEmpty(true);
+      setErrorMessage('Server is not responding');
+    }
+  }
+  
+  useEffect(() => {
+    info();
+    getUnregisteredCourses();
+  }, []);
 
-    const enroll = async (e) => {
-      let val;
-      if(!e || e == undefined) {
-        val = courses[0].code;
-      }else {
-        val = e.target.value;
-      }
-      const url = baseUrl + "/auth/student_register_course";
-      try {
-        const dataForm = new FormData();
-        dataForm.append('courses', val)
-        const post = await postApi(url, dataForm, localStorage.getItem('user_token'));
-      } catch(err) {
-        setEmpty(true);
-        setErrorMessage('Server is not responding');
-      }
-    } 
   return (
     <div>
         <Navbar name={name} />
@@ -58,9 +57,7 @@ const Student = () => {
         <span>course name</span>
         <select onChange={(e) => setEnrollCourse(e)} className="btn">
           {
-             courses.map((course) => {
-              return <option  value={course.code}>{course.code}</option>
-            })
+             courses.map(course => <option value={course.code}>{course.code}</option>)
           }
         </select>
         <button onClick={() => enroll(enrollCourse)} className="btn">Enroll</button>
--- frontend/src/components/WaitingSection.js
+++ frontend/src/components/WaitingSection.js
@@ -1,4 +1,5 @@
 
+
 const WaitingSection = () => {
   return (
     <section>
